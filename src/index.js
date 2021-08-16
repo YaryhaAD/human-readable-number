c4cc13ae5fdc476906ce475bb3ab7bb84ea74906
YaryhaAD <YaryhaAD@gmail.com>
2021-08-16 23:05:03 +0300

    feat: create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+var a = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+var b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
 module.exports = function toReadable (number) {
-  
+   if (number === 0) {
+        console.log('zero');
+        return 'zero';
+    }
+    if ((number = number.toString()).length > 3) {
+        return 'tooMany';
+    }
+    n = ('000' + number).substr(-3).match(/^(\d{1})(\d{2})$/);
+    if (!n) {
+        return;
+    }
+    var str = '';
+    if (n[1] != 0){
+        str = str + (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + ' hundred '
+    }
+    if (n[2] != 0){
+        str = str + (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]])
+    }
+    return str.trim();
 }
